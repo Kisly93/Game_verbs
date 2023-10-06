fbcb2d4f095b9afffaafaddf0f8234d91ec5a87e
Kisly93 <spsvk.kis.sov@gmail.com>
2023-10-06 16:49:16 +0200

    setup DialogFlow

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
 import logging
 
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from google.cloud import dialogflow
 from environs import Env
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+env = Env()
+env.read_env()
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
@@ -9,26 +13,46 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-def start(update, _):
-    update.message.reply_text('Здравствуйте')
+project_id = env('PROJECT_ID')
+
+def detect_intent_texts(session_id, text, language_code='ru-RU'):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    return response.query_result.fulfillment_text
+
+def start(update, context):
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        f'Здравстуйте {user.mention_markdown_v2()}\!',
+    )
 
 def help_command(update, _):
     update.message.reply_text('Help!')
 
 
-def echo(update, _):
-    update.message.reply_text(update.message.text)
+def handle_text(update, context):
+    user_id = update.message.from_user.id
+    user_message = update.message.text
+    bot_response = detect_intent_texts(str(user_id), user_message)
+
+    update.message.reply_text(bot_response)
 
 
 def main():
-    env = Env()
-    env.read_env()
-    updater = Updater(env('tg_token'))
-
+    updater = Updater(env('TG_TOKEN'))
     dispatcher = updater.dispatcher
+
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_text))
 
     updater.start_polling()
     updater.idle()
