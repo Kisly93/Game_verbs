655702b3c774bf46682fec150d1b77af83c082be
Kisly93 <spsvk.kis.sov@gmail.com>
2023-10-06 18:47:50 +0200

    added file for api key dialogflow

---
+++ create_dialogflow_api_key.py
@@ -0,0 +1,36 @@
+from environs import Env
+
+from google.cloud import api_keys_v2
+from google.cloud.api_keys_v2 import Key
+
+
+def create_api_key(project_id: str, suffix: str) -> Key:
+    client = api_keys_v2.ApiKeysClient()
+
+    key = api_keys_v2.Key()
+    key.display_name = f"My first API key - {suffix}"
+
+    # Initialize request and set arguments.
+    request = api_keys_v2.CreateKeyRequest()
+    request.parent = f"projects/{project_id}/locations/global"
+    request.key = key
+
+    # Make the request and wait for the operation to complete.
+    response = client.create_key(request=request).result()
+
+    print(f"Successfully created an API key: {response.name}")
+    # For authenticating with the API key, use the value in "response.key_string".
+    # To restrict the usage of this API key, use the value in "response.name".
+    return response
+
+
+if __name__ == '__main__':
+
+    env = Env()
+    env.read_env()
+    PROJECT_ID = env.str('PROJECT_ID')
+    SUFFIX = env.str('SUFFIX')
+
+    create_api_key(PROJECT_ID, SUFFIX)
+
+
