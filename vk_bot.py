711a42c8e3ed8ebe131bf47158b6ccb2c0e95670
Kisly93 <spsvk.kis.sov@gmail.com>
2023-10-11 16:03:49 +0200

    fixed vk_bot

--- vk_bot.py
+++ vk_bot.py
@@ -1,33 +1,20 @@
 import logging
 import random
-
 import telegram
 from environs import Env
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 from dialogflow import detect_intent_texts
-
-
-class TelegramLogHandler(logging.Handler):
-    def __init__(self, bot, chat_id):
-        super().__init__()
-        self.chat_id = chat_id
-        self.bot = bot
-
-    def emit(self, record):
-        log_message = self.format(record)
-        self.bot.send_message(chat_id=self.chat_id, text=log_message)
+from TelegramLogHandler import TelegramLogHandler
 
 
 def send_message(user_id, text, vk_api):
-    try:
-        vk_api.messages.send(
-            user_id=user_id,
-            message=text,
-            random_id=random.randint(1, 1000)
-        )
-    except Exception as e:
-        bot.logger.exception(f'Произошла ошибка: {e}')
+    vk_api.messages.send(
+        user_id=user_id,
+        message=text,
+        random_id=random.randint(1, 1000)
+    )
+
 
 
 def main():
@@ -37,9 +24,11 @@ def main():
     telegram_token = env('TELEGRAM_TOKEN')
     bot = telegram.Bot(token=telegram_token)
     bot.logger.addHandler(TelegramLogHandler(bot, chat_id))
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(levelname)s | %(name)s | %(message)s')
     bot.logger.warning('vk - Бот запущен')
     vk_token = env('VK_TOKEN')
     project_id = env('PROJECT_ID')
+
     try:
         vk_session = vk.VkApi(token=vk_token)
         vk_api = vk_session.get_api()
@@ -48,16 +37,15 @@ def main():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
                 session_id = f'vk-{event.user_id}'
                 user_message = event.text
-                text = detect_intent_texts(session_id, event.text, project_id)
-                if text is None:
+                text_answer, fallback = detect_intent_texts(session_id, event.text, project_id)
+                if fallback:
                     bot.logger.info(f'Сообщение пользователя: {user_message}')
                 else:
-                    send_message(event.user_id, text, vk_api)
+                    send_message(event.user_id, text_answer, vk_api)
 
     except Exception as e:
         bot.logger.exception(f'Произошла ошибка: {e}')
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(levelname)s | %(name)s | %(message)s')
     main()
