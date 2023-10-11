6e3279677b4cbd431c98ed2818f92ac88561630f
Kisly93 <spsvk.kis.sov@gmail.com>
2023-10-11 16:03:55 +0200

    fixed tg_bot

--- tg_bot.py
+++ tg_bot.py
@@ -4,17 +4,7 @@ import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from environs import Env
 from dialogflow import detect_intent_texts
-
-
-class TelegramLogHandler(logging.Handler):
-    def __init__(self, bot, chat_id):
-        super().__init__()
-        self.chat_id = chat_id
-        self.bot = bot
-
-    def emit(self, record):
-        log_message = self.format(record)
-        self.bot.send_message(chat_id=self.chat_id, text=log_message)
+from TelegramLogHandler import TelegramLogHandler
 
 
 def start(update, context):
@@ -22,36 +12,40 @@ def start(update, context):
     update.message.reply_markdown_v2(f'Здравствуйте {user.mention_markdown_v2()}')
 
 
+def handle_text(update, context, project_id):
+    session_id = f'tg-{update.message.from_user.id}'
+    text = update.message.text
+    text_answer, fallback = detect_intent_texts(str(session_id), text, project_id)
+    if text_answer:
+        update.message.reply_text(text_answer)
+    else:
+        update.message.reply_text(fallback)
+
+
 def main():
-    env = Env()
-    env.read_env()
-    project_id = env('PROJECT_ID')
-    chat_id = env('CHAT_ID')
-    telegram_token = env('TELEGRAM_TOKEN')
-    bot = telegram.Bot(token=telegram_token)
-    bot.logger.addHandler(TelegramLogHandler(bot, chat_id))
-    bot.logger.warning('tg - Бот запущен')
+    try:
+        env = Env()
+        env.read_env()
+        project_id = env('PROJECT_ID')
+        chat_id = env('CHAT_ID')
+        telegram_token = env('TELEGRAM_TOKEN')
+        bot = telegram.Bot(token=telegram_token)
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(levelname)s | %(name)s | %(message)s')
+        bot.logger.addHandler(TelegramLogHandler(bot, chat_id))
+        bot.logger.warning('tg - Бот запущен')
 
-    def handle_text(update, context):
-        try:
-            session_id = f'tg-{update.message.from_user.id}'
-            text = update.message.text
-            bot_response = detect_intent_texts(str(session_id), text, project_id)
-            if bot_response:
-                update.message.reply_text(bot_response)
-            else:
-                update.message.reply_text("Извините, не могу понять ваш запрос.")
-        except Exception as e:
-            bot.logger.exception(f'Произошла ошибка: {e}')
+        updater = Updater(env('TG_TOKEN'))
+        dispatcher = updater.dispatcher
 
-    updater = Updater(env('TG_TOKEN'))
-    dispatcher = updater.dispatcher
+        dispatcher.add_handler(CommandHandler("start", start))
+        dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command,
+                                              lambda update, context: handle_text(update, context, project_id)))
 
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_text))
+        updater.start_polling()
+        updater.idle()
 
-    updater.start_polling()
-    updater.idle()
+    except Exception as e:
+        logging.exception(f'Произошла ошибка в функции main: {e}')
 
 
 if __name__ == '__main__':
